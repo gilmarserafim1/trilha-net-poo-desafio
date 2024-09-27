3fed4046df04a9a4eef237edd4b9f369087e03e2
Gilmar <gilmarjunior72@gmail.com>
2024-09-26 22:36:14 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("+55(11)99999-9999", "G11 Plus", "654684546548", 64);
+Iphone iphone = new Iphone("+55(11)99000-0000", "16 Pró Max", "84946565468", 256);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
